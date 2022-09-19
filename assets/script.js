dd3642179ac2a441893aea5c82e922755b3185e6
Henry santos <henrryfps@gmail.com>
2022-09-19 19:23:19 -0400

    commit final1

--- assets/script.js
+++ assets/script.js
@@ -1,62 +1,68 @@
-let slide = document.querySelectorAll('.slide-container')
-let index = 0
+let slide = document.querySelectorAll(".slide-container");
+let index = 0;
 
-const leftButton = document.getElementById('prev');
-const rigthButton = document.getElementById('next');
+const leftButton = document.getElementById("prev");
+const rigthButton = document.getElementById("next");
 
-function next(){
-    slide[index].classList.remove('active')
-    index = (index + 1) % slide.length
-    slide[index].classList.add('active')
+function next() {
+  slide[index].classList.remove("active");
+  index = (index + 1) % slide.length;
+  slide[index].classList.add("active");
 }
 
 leftButton.addEventListener("click", () => {
-    next()
-})
+  next();
+});
 
-function prev(){
-    slide[index].classList.remove('active')
-    index = (index - 1 + slide.length) % slide.length
-    slide[index].classList.add('active')
+function prev() {
+  slide[index].classList.remove("active");
+  index = (index - 1 + slide.length) % slide.length;
+  slide[index].classList.add("active");
 }
 
 rigthButton.addEventListener("click", () => {
-    prev()
+  prev();
+});
+
+setInterval(next, 8000);
+
+//MENU HAMBURGUER//
+
+let show = true
+
+const menuSection = document.querySelector(".menu-section")
+const menuBurguer = menuSection.querySelector(".menu-burguer")
+
+menuBurguer.addEventListener("click", () => {
+    menuSection.classList.toggle("on", show)
+    show = !show
 })
-
-
-setInterval (next, 8000)
-
 
 //SCROLL ANIMAÇÃO//
 
-// const sr = ScrollReveal({
-//     distance: '60px',
-//     duration: 1900,
-//     reset: true,
-// });
+const sr = ScrollReveal({
+  distance: "60px",
+  duration: 1900,
+  reset: true,
+});
 
+sr.reveal(`.content-home`, {
+  origin: "left",
+  interval: 200,
+});
 
-//  sr.reveal(`.content-home`, {
-//      origin: 'left',
-//      interval: 200,
-// })
+sr.reveal(`.content-card`, {
+  origin: "right",
+});
 
+sr.reveal(`.container-text`, {
+  origin: "left",
+});
 
-// sr.reveal(`.content-card`, {
-//     origin: 'right'
-// })
+sr.reveal(`.container-img`, {
+  origin: "right",
+});
 
-// sr.reveal(`.container-text`, {
-//     origin: 'left'
-// })
-
-// sr.reveal(`.container-img`, {
-//     origin: 'right'
-// })
-
-// sr.reveal(`.content-contato`, {
-//     origin: 'bottom'
-// })
-
-
+sr.reveal(`.content-contato`, {
+  origin: "bottom",
+});
